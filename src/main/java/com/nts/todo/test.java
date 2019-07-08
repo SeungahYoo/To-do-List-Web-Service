0b2064314ab337afa28f42acbc0af3ebb918e927
Seungah Yoo <sayoo0704@gmail.com>
2019-07-08 17:52:17 +0900

    test

--- src/main/java/com/nts/todo/test.java
+++ src/main/java/com/nts/todo/test.java
@@ -3,13 +3,13 @@ package com.nts.todo;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.nts.todo.dao.todoDAO;
-import com.nts.todo.dto.todo;
+import com.nts.todo.dao.TodoDAO;
+import com.nts.todo.dto.Todo;
 
 public class test {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		todoDAO dao = new todoDAO();
-		List<todo> ret = dao.getTodos("TODO");
+		TodoDAO dao = new TodoDAO();
+		List<Todo> ret = dao.getTodos("TODO");
 		System.out.println(ret);
 	}
 }
