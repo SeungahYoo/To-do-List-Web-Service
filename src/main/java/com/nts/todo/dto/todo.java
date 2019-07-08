56bae13883b8f051cb56e2e04c2c6468e324a698
Seungah Yoo <sayoo0704@gmail.com>
2019-07-08 14:08:30 +0900

    dto, dao

---
+++ src/main/java/com/nts/todo/dto/todo.java
@@ -0,0 +1,78 @@
+package com.nts.todo.dto;
+
+import java.time.LocalDateTime;
+
+public class todo {
+	private long id;
+	private String name;
+	private LocalDateTime regdate;
+	private int sequence;
+	private String title;
+	private String type;
+
+	public todo() {
+		super();
+	}
+
+	public todo(String name, int sequence, String title) {
+		super();
+		this.name = name;
+		this.sequence = sequence;
+		this.title = title;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(LocalDateTime regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "todo [id=" + id + ", name=" + name + ", regdate=" + regdate + ", sequence=" + sequence + ", title="
+			+ title + ", type=" + type + "]";
+	}
+
+}
