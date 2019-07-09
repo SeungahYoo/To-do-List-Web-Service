950427ce6f72513956111749258c73f5fad3462f
Seungah Yoo <sayoo0704@gmail.com>
2019-07-09 10:55:23 +0900

    setter param 가공 로직 추가

--- src/main/java/com/nts/todo/dao/TodoDAO.java
+++ src/main/java/com/nts/todo/dao/TodoDAO.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,37 +14,39 @@ public class TodoDAO {
 	private static String DB_URL = "jdbc:mysql://10.113.116.52:13306/user10?useSSL=false";
 	private static String DB_USER = "user10";
 	private static String DB_PASSWORD = "user10";
-	private static Connection CONN = null;
 
-	public TodoDAO() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		CONN = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+	public TodoDAO() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public List<Todo> getTodos(String type) throws SQLException {
 		System.out.println(type);
-		ArrayList<Todo> todos = new ArrayList<>();
+		List<Todo> todos = new ArrayList<>();
 		String sql = "SELECT * FROM todo WHERE type = ?";
 
-		try (PreparedStatement ps = CONN.prepareStatement(sql);) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			PreparedStatement ps = conn.prepareStatement(sql)) {
 			ps.setString(1, type);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Todo selectedTodo = new Todo();
-				selectedTodo.setId(rs.getLong("id"));
-				selectedTodo.setTitle(rs.getString("title"));
-				selectedTodo.setName(rs.getString("name"));
-				selectedTodo.setSequence(rs.getInt("sequence"));
-				selectedTodo.setType(rs.getString("type"));
-
-				//Timestamp -> LocalDateTime type casting
-				Timestamp tmpDate = rs.getTimestamp("regdate");
-				LocalDateTime dateTime = tmpDate.toLocalDateTime();
-				selectedTodo.setRegdate(dateTime);
-
-				todos.add(selectedTodo);
+			try (ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					Todo selectedTodo = new Todo();
+					selectedTodo.setId(rs.getLong("id"));
+					selectedTodo.setTitle(rs.getString("title"));
+					selectedTodo.setName(rs.getString("name"));
+					selectedTodo.setSequence(rs.getInt("sequence"));
+					selectedTodo.setType(rs.getString("type"));
+					selectedTodo.setRegdate(rs.getTimestamp("regdate"));
+					todos.add(selectedTodo);
+				}
 			}
 		}
+
+		System.out.println("hello");
 		return todos;
 	}
 
@@ -54,7 +54,8 @@ public class TodoDAO {
 		int result = 0;
 
 		String sql = "INSERT INTO todo(title, name, sequence) VALUES(?, ?, ?)";
-		try (PreparedStatement ps = CONN.prepareStatement(sql);) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			PreparedStatement ps = conn.prepareStatement(sql);) {
 			ps.setString(1, todo.getTitle());
 			ps.setString(2, todo.getName());
 			ps.setInt(3, todo.getSequence());
@@ -68,7 +69,8 @@ public class TodoDAO {
 		String currentType = todo.getType();
 		String sql = "UPDATE todo SET type = 'DOING' WHERE id = ?";
 		//sql = "update todo set type = 'DONE' where id = ?";
-		try (PreparedStatement ps = CONN.prepareStatement(sql);) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			PreparedStatement ps = conn.prepareStatement(sql);) {
 			ps.setLong(1, todo.getId());
 			result = ps.executeUpdate();
 		}
